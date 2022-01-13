9ccd7aa27f12c4f72e7fd1d089c1725634bb0d33
Chanda Abdul <56906759+Chanda-Abdul@users.noreply.github.com>
2022-01-13 01:26:31 -0800

    Add files via upload

---
+++ Solidity Inbox Project/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "miss enact cannon empower hint holiday detect pitch gas core lounge motion",
+  "https://rinkeby.infura.io/v3/91b87453ed7646e68e950929f55d7b55"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+
+deploy();
