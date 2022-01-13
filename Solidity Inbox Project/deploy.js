49c89e094aee4cdda1d60b48d785c2d8d1468301
Chanda Abdul <56906759+Chanda-Abdul@users.noreply.github.com>
2022-01-13 01:42:09 -0800

    Add files via upload

--- Solidity Inbox Project/deploy.js
+++ Solidity Inbox Project/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, evm } = require("./compile");
 
 const provider = new HDWalletProvider(
   "miss enact cannon empower hint holiday detect pitch gas core lounge motion",
@@ -14,11 +14,12 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
     .send({ gas: "1000000", from: accounts[0] });
 
   console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
 };
 
 deploy();
--- Solidity Inbox Project/test/Inbox.test.js
+++ Solidity Inbox Project/test/Inbox.test.js
@@ -4,10 +4,11 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 //web3 instance
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require("../compile");
+const { abi, evm } = require("../compile");
 
 let accounts;
 let inbox;
+
 const INITIAL_STRING = "Hi there!";
 
 beforeEach(async () => {
@@ -15,9 +16,9 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   //Use one of those accounts to deploy
   //the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: [INITIAL_STRING],
     })
     .send({ from: accounts[0], gas: "1000000" });
